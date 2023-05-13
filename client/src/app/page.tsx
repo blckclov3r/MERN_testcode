12f6f83f12e7785e04fbd97507dcd581a985e342
blckclov3r <blckclov3r@gmail.com>
2023-05-13 23:41:24 +0800

    apply muiv5  in client directory

---
+++ client/src/app/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import {Container, Typography} from "@mui/material";
+
+export default function Home() {
+    return (
+        <main>
+            <Container>
+                <Typography variant={'h1'}>Helloworld</Typography>
+            </Container>
+        </main>
+    )
+}
