436380d633a3715faebe34626d94b43fd85eb17f
blckclov3r <blckclov3r@gmail.com>
2023-05-24 00:22:49 +0800

    fix client modal

--- client/src/components/clients/ClientModal.tsx
+++ client/src/components/clients/ClientModal.tsx
@@ -5,15 +5,24 @@ import {useMutation, useQuery} from "@apollo/client";
 import clients from "@/queries/clients";
 
 interface Client {
-    name?: string;
-    email?: string;
-    phone?: string;
+    name: string;
+    email: string;
+    phone: string;
 }
 
 interface AddClientProps {
     handleClose: () => void
     open: boolean
-    title?: string
+    id?: string
+    handleKeyDown: (event: React.KeyboardEvent<HTMLDivElement>) => void
+    handleSubmit: (event: FormEvent) => Promise<void>
+    handleNameChange: (event: ChangeEvent<HTMLInputElement>) => void
+    handleEmailChange: (event: ChangeEvent<HTMLInputElement>) => void
+    handlePhoneChange: (event: ChangeEvent<HTMLInputElement>) => void
+    name: string | null
+    email: string | null
+    phone: string | null
+    dataClient: { client: Client } | undefined
 }
 
 const style = {
@@ -29,99 +38,6 @@ const style = {
 
 const ClientModal: React.FC<AddClientProps> = (props) => {
 
-    const [name, setName] = useState<string | null>('');
-    const [email, setEmail] = useState<string | null>('');
-    const [phone, setPhone] = useState<string | null>('');
-
-    const {ADD_CLIENT, GET_CLIENTS, GET_CLIENTID, UPDATE_CLIENT} = clients();
-    const id = props?.title ?? null
-
-    const {data, loading} = useQuery<{ client: Client }>(GET_CLIENTID, {
-        variables: {
-            id
-        },
-        skip: props.title === "" || props.title === null,
-        ssr: true
-    })
-
-    useEffect(() => {
-        if (loading && id) {
-            setName(null);
-            setEmail(null);
-            setPhone(null);
-        }
-    }, [id, loading]);
-
-    const [addClient] = useMutation(ADD_CLIENT, {
-        variables: {
-            name,
-            email,
-            phone
-        },
-        update(cache, {
-            data: {addClient}
-        }) {
-            const {clients} = cache.readQuery<any>({query: GET_CLIENTS})
-            cache.writeQuery({
-                query: GET_CLIENTS,
-                data: {clients: clients?.concat([addClient]) ?? []}
-            })
-        }
-        // refetchQueries: [{query: GET_CLIENTS}]
-    });
-
-    const [updateClient] = useMutation(UPDATE_CLIENT, {
-        variables: {
-            id,
-            ...((name !== null) ? {name} : {}),
-            ...((email !== null) ? {email} : {}),
-            ...((phone !== null) ? {phone} : {}),
-        },
-    });
-
-    const handleNameChange = (event: ChangeEvent<HTMLInputElement>) => {
-        setName(event.target.value);
-    };
-
-    const handleEmailChange = (event: ChangeEvent<HTMLInputElement>) => {
-        setEmail(event.target.value);
-    };
-
-    const handlePhoneChange = (event: ChangeEvent<HTMLInputElement>) => {
-        setPhone(event.target.value);
-    };
-
-    const handleSubmit = async (event: FormEvent) => {
-        event.preventDefault();
-        try {
-            if (id === "Add Client") {
-                if(!name || !phone || !email){
-                    alert('Please fill in all fields')
-                }
-                await addClient().then(() => {
-                    setName(null)
-                    setEmail(null)
-                    setPhone(null)
-                })
-            } else {
-                await updateClient().then(() => {
-                    setName(null)
-                    setEmail(null)
-                    setPhone(null)
-                })
-            }
-            props.handleClose();
-        } catch (err) {
-            console.log('@@err', err)
-        }
-    };
-
-    const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
-        if (event.key === 'Escape') {
-            event.stopPropagation();
-        }
-    };
-
     return (
         <>
             <Modal
@@ -130,44 +46,44 @@ const ClientModal: React.FC<AddClientProps> = (props) => {
                 aria-labelledby="modal-modal-title"
                 aria-describedby="modal-modal-description"
             >
-                <div onKeyDown={handleKeyDown}>
+                <div onKeyDown={props.handleKeyDown}>
                     <Box sx={style}>
                         <Typography sx={{mb: 1}} id="modal-modal-title" variant="h6" component="h2">
-                            {props.title}
+                            {props.id}
                         </Typography>
-                        <form onSubmit={handleSubmit}>
+                        <form onSubmit={props.handleSubmit}>
                             <TextField
                                 label="Name"
                                 variant="outlined"
-                                value={name ? name : undefined}
-                                onChange={handleNameChange}
+                                value={props.name ? props.name : undefined}
+                                onChange={props.handleNameChange}
                                 fullWidth
-                                defaultValue={data?.client.name ?? undefined}
+                                defaultValue={props.dataClient?.client.name ?? undefined}
                                 margin="normal"
                                 size="small"
-                                InputLabelProps={{shrink: (!!name || data?.client.name) as boolean}}
+                                InputLabelProps={{shrink: (!!props.name || props.dataClient?.client.name) as boolean}}
                             />
                             <TextField
                                 label="Email"
                                 variant="outlined"
-                                value={email ? email : undefined}
-                                onChange={handleEmailChange}
+                                value={props.email ? props.email : undefined}
+                                onChange={props.handleEmailChange}
                                 fullWidth
                                 margin="normal"
                                 size="small"
-                                defaultValue={data?.client.email ?? undefined}
-                                InputLabelProps={{shrink: (!!email || data?.client.email) as boolean}}
+                                defaultValue={props.dataClient?.client.email ?? undefined}
+                                InputLabelProps={{shrink: (!!props.email || props.dataClient?.client.email) as boolean}}
                             />
                             <TextField
                                 label="Phone"
                                 variant="outlined"
-                                value={phone ? phone : undefined}
-                                onChange={handlePhoneChange}
+                                value={props.phone ? props.phone : undefined}
+                                onChange={props.handlePhoneChange}
                                 fullWidth
                                 margin="normal"
                                 size="small"
-                                defaultValue={data?.client.phone ?? undefined}
-                                InputLabelProps={{shrink: (!!phone || data?.client.phone) as boolean}}
+                                defaultValue={props.dataClient?.client.phone ?? undefined}
+                                InputLabelProps={{shrink: (!!props.phone || props.dataClient?.client.phone) as boolean}}
                             />
 
                             <Box sx={{display: 'flex', justifyContent: 'space-between', mt: 3}}>
--- client/src/components/clients/ClientsContainer.tsx
+++ client/src/components/clients/ClientsContainer.tsx
@@ -1,28 +1,103 @@
 import {Box, Paper, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography} from "@mui/material";
-import {useQuery} from "@apollo/client";
+import {useMutation, useQuery} from "@apollo/client";
 import clients from "@/queries/clients"
 import Table from '@mui/material/Table';
 import ClientRow from "@/components/clients/ClientRow";
 import {Client} from "@/generated/graphql";
 import Button from "@mui/material/Button";
-import {useState} from "react";
+import {ChangeEvent, FormEvent, useEffect, useState} from "react";
 import ClientModal from "@/components/clients/ClientModal";
 
 const ClientsContainer = () => {
-    const {GET_CLIENTS, DELETE_CLIENT} = clients()
-    const {data, loading, error} = useQuery(GET_CLIENTS)
-
+    const [id, setId] = useState<string | null>(null)
     const [open, setOpen] = useState(false);
-    const [title, setTitle] = useState<string>("")
+    const [name, setName] = useState<string | null>(null);
+    const [email, setEmail] = useState<string | null>(null);
+    const [phone, setPhone] = useState<string | null>(null);
+    const {ADD_CLIENT, GET_CLIENTS, GET_CLIENTID, UPDATE_CLIENT, DELETE_CLIENT} = clients();
+    const {data: dataClients} = useQuery(GET_CLIENTS)
+    const {data: dataClient} = useQuery(GET_CLIENTID, {
+        variables: {
+            id
+        },
+        skip: id === "" || id === null,
+    })
+
+    const [addClient] = useMutation(ADD_CLIENT, {
+        variables: {
+            name,
+            email,
+            phone
+        },
+        update(cache, {
+            data: {addClient}
+        }) {
+            const {clients} = cache.readQuery<any>({query: GET_CLIENTS})
+            cache.writeQuery({
+                query: GET_CLIENTS,
+                data: {clients: clients?.concat([addClient]) ?? []}
+            })
+        }
+        // refetchQueries: [{query: GET_CLIENTS}]
+    });
+
+    const [updateClient] = useMutation(UPDATE_CLIENT, {
+        variables: {
+            id,
+            ...((name !== null) ? {name} : {}),
+            ...((email !== null) ? {email} : {}),
+            ...((phone !== null) ? {phone} : {}),
+        },
+    });
+
+    const handleNameChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setName(event.target.value);
+    };
+
+    const handleEmailChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setEmail(event.target.value);
+    };
+
+    const handlePhoneChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setPhone(event.target.value);
+    };
+
+    const handleSubmit = async (event: FormEvent) => {
+        event.preventDefault();
+        try {
+            if (id === "" || id === null) {
+                if (!name || !phone || !email) {
+                    alert('Please fill in all fields')
+                }
+                await addClient()
+            } else {
+                await updateClient()
+            }
+            setId(null)
+            setName(null)
+            setEmail(null)
+            setPhone(null)
+            handleClose();
+        } catch (err) {
+            console.log('@@err', err)
+        }
+    };
+
+    const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
+        if (event.key === 'Escape') {
+            event.stopPropagation();
+        }
+    };
+
     const handleOpen = (id?: string) => {
         if (id) {
-            setTitle(id)
+            setId(id)
         }
         setOpen(true);
     }
     const handleClose = () => {
         setOpen(false)
-        setTitle("")
+        setId(null)
     }
 
     return (
@@ -34,40 +109,46 @@ const ClientsContainer = () => {
                 handleOpen()
             }} sx={{mb: 2}}>Add Client</Button>
             {
-                loading ? (
-                    <Box>
-                        <Typography variant={'body1'}>Loading...</Typography>
-                    </Box>
-                ) : (
-                    <TableContainer component={Paper}>
-                        <Table sx={{minWidth: 650}} aria-label="simple table">
-                            <TableHead>
-                                <TableRow>
-                                    <TableCell>id</TableCell>
-                                    <TableCell>name</TableCell>
-                                    <TableCell>email</TableCell>
-                                    <TableCell>phone</TableCell>
-                                    <TableCell></TableCell>
-                                </TableRow>
-                            </TableHead>
-                            <TableBody>
-                                {
-                                    data?.clients?.map((client: Client) => (
-                                        <TableRow
-                                            key={client.id}
-                                            sx={{'&:last-child td, &:last-child th': {border: 0}}}
-                                        >
-                                            <ClientRow handleOpen={handleOpen}
-                                                       client={client}/>
-                                        </TableRow>
-                                    ))
-                                }
-                            </TableBody>
-                        </Table>
-                    </TableContainer>
-                )
+                <TableContainer component={Paper}>
+                    <Table sx={{minWidth: 650}} aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>id</TableCell>
+                                <TableCell>name</TableCell>
+                                <TableCell>email</TableCell>
+                                <TableCell>phone</TableCell>
+                                <TableCell></TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {
+                                dataClients?.clients?.map((client: Client) => (
+                                    <TableRow
+                                        key={client.id}
+                                        sx={{'&:last-child td, &:last-child th': {border: 0}}}
+                                    >
+                                        <ClientRow handleOpen={handleOpen}
+                                                   client={client}/>
+                                    </TableRow>
+                                ))
+                            }
+                        </TableBody>
+                    </Table>
+                </TableContainer>
             }
-            <ClientModal title={title ? title : 'Add Client'} handleClose={handleClose} open={open}/>
+            <ClientModal id={id ? id : 'Add Client'}
+                         handleClose={handleClose}
+                         open={open}
+                         handleKeyDown={handleKeyDown}
+                         handleSubmit={handleSubmit}
+                         handleNameChange={handleNameChange}
+                         handleEmailChange={handleEmailChange}
+                         handlePhoneChange={handlePhoneChange}
+                         name={name}
+                         email={email}
+                         phone={phone}
+                         dataClient={dataClient}
+            />
         </>
     )
 }
