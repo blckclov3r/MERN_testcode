8cd84f0672be3726f3274ddb42356a382190d011
blckclov3r <blckclov3r@gmail.com>
2023-05-24 01:51:01 +0800

    commit changes

--- client/src/components/projects/ProjectModal.tsx
+++ client/src/components/projects/ProjectModal.tsx
@@ -1,166 +1,180 @@
-import {Box, MenuItem, Modal, Select, SelectChangeEvent, TextField, Typography,} from "@mui/material";
-import {ChangeEvent, Dispatch, FormEvent, SetStateAction} from "react";
+import {
+  Box,
+  MenuItem,
+  Modal,
+  Select,
+  SelectChangeEvent,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { ChangeEvent, Dispatch, FormEvent, SetStateAction } from "react";
 import Button from "@mui/material/Button";
-import {IClients, IProject, SelectOption,} from "@/components/projects/ProjectsContainer";
+import {
+  IClients,
+  IProject,
+  SelectOption,
+} from "@/components/projects/ProjectsContainer";
 
 interface AddProjectProps {
-    id: string | null;
-    handleClose: () => void;
-    open: boolean;
-    setClientId: Dispatch<SetStateAction<string | null>>;
-    setName: Dispatch<SetStateAction<string | null>>;
-    setDescription: Dispatch<SetStateAction<string | null>>;
-    setStatus: Dispatch<SetStateAction<string | null>>;
-    clientId: string | null;
-    name: string | null;
-    description: string | null;
-    status: string | null;
-    addProject: any;
-    updateProject: any;
-    projectList: { project: IProject } | undefined;
-    clientsList: { clients: IClients[] } | undefined;
-    clientIdOptions: SelectOption[];
-    handleKeyDown: (event: React.KeyboardEvent<HTMLDivElement>) => void;
-    handleStatusChange: (event: SelectChangeEvent) => void;
-    handleClientChange: (event: SelectChangeEvent) => Promise<void>;
-    selectedStatus: string;
-    handleNameChange: (event: ChangeEvent<HTMLInputElement>) => void;
-    handleDescriptionChange: (event: ChangeEvent<HTMLInputElement>) => void;
-    handleSubmit: (event: FormEvent) => Promise<void>;
+  id: string | null;
+  handleClose: () => void;
+  open: boolean;
+  setClientId: Dispatch<SetStateAction<string | null>>;
+  setName: Dispatch<SetStateAction<string | null>>;
+  setDescription: Dispatch<SetStateAction<string | null>>;
+  setStatus: Dispatch<SetStateAction<string | null>>;
+  clientId: string | null;
+  name: string | null;
+  description: string | null;
+  status: string | null;
+  addProject: any;
+  updateProject: any;
+  projectList: { project: IProject } | undefined;
+  clientsList: { clients: IClients[] } | undefined;
+  clientIdOptions: SelectOption[];
+  handleKeyDown: (event: React.KeyboardEvent<HTMLDivElement>) => void;
+  handleStatusChange: (event: SelectChangeEvent) => void;
+  handleClientChange: (event: SelectChangeEvent) => Promise<void>;
+  selectedStatus: string;
+  handleNameChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  handleDescriptionChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  handleSubmit: (event: FormEvent) => Promise<void>;
 }
 
 const style = {
-    position: "absolute" as "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    width: 400,
-    bgcolor: "background.paper",
-    p: 4,
-    border: "none",
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  p: 4,
+  border: "none",
 };
 
 const ProjectModal: React.FC<AddProjectProps> = (props) => {
-    return (
-        <>
-            <Modal
-                open={props.open}
-                onClose={props.handleClose}
-                aria-labelledby="modal-modal-title"
-                aria-describedby="modal-modal-description"
+  return (
+    <>
+      <Modal
+        open={props.open}
+        onClose={props.handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <div onKeyDown={props.handleKeyDown}>
+          <Box sx={style}>
+            <Typography
+              sx={{ mb: 1 }}
+              id="modal-modal-title"
+              variant="h6"
+              component="h2"
             >
-                <div onKeyDown={props.handleKeyDown}>
-                    <Box sx={style}>
-                        <Typography
-                            sx={{mb: 1}}
-                            id="modal-modal-title"
-                            variant="h6"
-                            component="h2"
-                        >
-                            {props.id ? props.id : "Add Project"}
-                        </Typography>
-                        <form onSubmit={props.handleSubmit}>
-                            <Select
-                                labelId={"clientId"}
-                                id={"clientId"}
-                                onChange={props.handleClientChange}
-                                value={
-                                    (props?.clientId || props.projectList?.project.clientId) ?? null
-                                }
-                                defaultValue={props.projectList?.project.clientId ?? ''}
-                                size={"small"}
-                                sx={{my: 1}}
-                                fullWidth={true}
-                            >
-                                {props.clientsList?.clients.map((client) => (
-                                    <MenuItem key={client.id} value={client.id}>
-                                        {client.name}
-                                    </MenuItem>
-                                ))}
-                            </Select>
+              {props.id ? props.id : "Add Project"}
+            </Typography>
+            <form onSubmit={props.handleSubmit}>
+              <Select
+                labelId={"clientId"}
+                id={"clientId"}
+                onChange={props.handleClientChange}
+                value={
+                  (props?.clientId ||
+                    (props.projectList?.project.clientId ?? "")) ??
+                  null
+                }
+                defaultValue={props.projectList?.project.clientId ?? ""}
+                size={"small"}
+                sx={{ my: 1 }}
+                fullWidth={true}
+              >
+                {props.clientsList?.clients.map((client) => (
+                  <MenuItem key={client.id} value={client.id}>
+                    {client.name}
+                  </MenuItem>
+                ))}
+              </Select>
 
-                            <TextField
-                                label="Name"
-                                variant="outlined"
-                                value={props.name ? props.name : undefined}
-                                onChange={props.handleNameChange}
-                                fullWidth
-                                defaultValue={props.projectList?.project?.name ?? undefined}
-                                margin="normal"
-                                size="small"
-                                InputLabelProps={{
-                                    shrink: (!!props.name ||
-                                        props.projectList?.project?.name) as boolean,
-                                }}
-                            />
-                            <TextField
-                                label="Description"
-                                variant="outlined"
-                                value={props.description ? props.description : undefined}
-                                onChange={props.handleDescriptionChange}
-                                fullWidth
-                                margin="normal"
-                                size="small"
-                                defaultValue={
-                                    props.projectList?.project?.description ?? undefined
-                                }
-                                InputLabelProps={{
-                                    shrink: (!!props.description ||
-                                        props.projectList?.project?.description) as boolean,
-                                }}
-                            />
+              <TextField
+                label="Name"
+                variant="outlined"
+                value={props.name ? props.name : undefined}
+                onChange={props.handleNameChange}
+                fullWidth
+                defaultValue={props.projectList?.project?.name ?? undefined}
+                margin="normal"
+                size="small"
+                InputLabelProps={{
+                  shrink: (!!props.name ||
+                    props.projectList?.project?.name) as boolean,
+                }}
+              />
+              <TextField
+                label="Description"
+                variant="outlined"
+                value={props.description ? props.description : undefined}
+                onChange={props.handleDescriptionChange}
+                fullWidth
+                margin="normal"
+                size="small"
+                defaultValue={
+                  props.projectList?.project?.description ?? undefined
+                }
+                InputLabelProps={{
+                  shrink: (!!props.description ||
+                    props.projectList?.project?.description) as boolean,
+                }}
+              />
 
-                            <Select
-                                labelId={"statusId"}
-                                id={"selectedStatusId"}
-                                value={
-                                    props.status ||
-                                    (props.selectedStatus !== null
-                                        ? props.selectedStatus
-                                        : props.clientIdOptions[0].value)
-                                }
-                                defaultValue={
-                                    (props.id
-                                        ? props.selectedStatus
-                                        : props.clientIdOptions[0].value) ??
-                                    props.clientIdOptions[0].value
-                                }
-                                onChange={props.handleStatusChange}
-                                size={"small"}
-                                sx={{my: 1}}
-                            >
-                                {props.clientIdOptions?.map((option) => (
-                                    <MenuItem key={option.value} value={option.value}>
-                                        {option.label}
-                                    </MenuItem>
-                                ))}
-                            </Select>
+              <Select
+                labelId={"statusId"}
+                id={"selectedStatusId"}
+                value={
+                  props.status ||
+                  (props.selectedStatus !== null
+                    ? props.selectedStatus
+                    : props.clientIdOptions[0].value)
+                }
+                defaultValue={
+                  (props.id
+                    ? props.selectedStatus
+                    : props.clientIdOptions[0].value) ??
+                  props.clientIdOptions[0].value
+                }
+                onChange={props.handleStatusChange}
+                size={"small"}
+                sx={{ my: 1 }}
+              >
+                {props.clientIdOptions?.map((option) => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.label}
+                  </MenuItem>
+                ))}
+              </Select>
 
-                            <Box
-                                sx={{display: "flex", justifyContent: "space-between", mt: 3}}
-                            >
-                                <Button
-                                    onClick={props.handleClose}
-                                    variant="contained"
-                                    color={"error"}
-                                    size={"small"}
-                                >
-                                    Cancel
-                                </Button>
-                                <Button
-                                    type="submit"
-                                    variant="contained"
-                                    color="primary"
-                                    size="small"
-                                >
-                                    Submit
-                                </Button>
-                            </Box>
-                        </form>
-                    </Box>
-                </div>
-            </Modal>
-        </>
-    );
+              <Box
+                sx={{ display: "flex", justifyContent: "space-between", mt: 3 }}
+              >
+                <Button
+                  onClick={props.handleClose}
+                  variant="contained"
+                  color={"error"}
+                  size={"small"}
+                >
+                  Cancel
+                </Button>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  color="primary"
+                  size="small"
+                >
+                  Submit
+                </Button>
+              </Box>
+            </form>
+          </Box>
+        </div>
+      </Modal>
+    </>
+  );
 };
 export default ProjectModal;
--- client/src/components/projects/ProjectsContainer.tsx
+++ client/src/components/projects/ProjectsContainer.tsx
@@ -1,252 +1,252 @@
 import {
-    Box,
-    Paper,
-    SelectChangeEvent,
-    TableBody,
-    TableCell,
-    TableContainer,
-    TableHead,
-    TableRow,
-    Typography,
+  Box,
+  Paper,
+  SelectChangeEvent,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
 } from "@mui/material";
-import {useMutation, useQuery} from "@apollo/client";
+import { useMutation, useQuery } from "@apollo/client";
 import Table from "@mui/material/Table";
-import {Project} from "@/generated/graphql";
+import { Project } from "@/generated/graphql";
 import Button from "@mui/material/Button";
 import projects from "@/queries/projects";
 import ProjectRow from "@/components/projects/ProjectRow";
-import {ChangeEvent, FormEvent, useCallback, useMemo, useState} from "react";
+import { ChangeEvent, FormEvent, useCallback, useMemo, useState } from "react";
 import ProjectModal from "@/components/projects/ProjectModal";
 import clients from "@/queries/clients";
 
 export interface IProject {
-    id: string;
-    name?: string;
-    description?: string;
-    status?: string;
-    clientId: string;
+  id: string;
+  name?: string;
+  description?: string;
+  status?: string;
+  clientId: string;
 }
 
 export interface IClients {
-    __typename: string;
-    id: string;
-    name: string;
-    email: string;
-    phone: string;
+  __typename: string;
+  id: string;
+  name: string;
+  email: string;
+  phone: string;
 }
 
 export type SelectOption = {
-    id: number;
-    value: string;
-    label: string;
+  id: number;
+  value: string;
+  label: string;
 };
 const clientIdOptions: SelectOption[] = [
-    {
-        id: 1,
-        value: "new",
-        label: "Not Started",
-    },
-    {
-        id: 2,
-        value: "progress",
-        label: "In Progress",
-    },
-    {
-        id: 3,
-        value: "completed",
-        label: "Completed",
-    },
+  {
+    id: 1,
+    value: "new",
+    label: "Not Started",
+  },
+  {
+    id: 2,
+    value: "progress",
+    label: "In Progress",
+  },
+  {
+    id: 3,
+    value: "completed",
+    label: "Completed",
+  },
 ];
 const ProjectContainer = () => {
-    const [id, setId] = useState<string | null>(null);
-    const [open, setOpen] = useState(false);
-    const [status, setStatus] = useState<string | null>(null);
-    const [clientId, setClientId] = useState<string | null>(null);
-    const [name, setName] = useState<string | null>(null);
-    const [description, setDescription] = useState<string | null>(null);
+  const [id, setId] = useState<string | null>(null);
+  const [open, setOpen] = useState(false);
+  const [status, setStatus] = useState<string | null>(null);
+  const [clientId, setClientId] = useState<string | null>(null);
+  const [name, setName] = useState<string | null>(null);
+  const [description, setDescription] = useState<string | null>(null);
 
-    const handleOpen = useCallback(
-        (id?: string) => {
-            setOpen(true);
-            if (id) {
-                setId(id);
-            }
-        },
-        [id]
-    );
+  const handleOpen = useCallback(
+    (id?: string) => {
+      setOpen(true);
+      if (id) {
+        setId(id);
+      }
+    },
+    [id]
+  );
 
-    const handleClose = useCallback(() => {
-        setOpen(false);
-        setId(null);
-    }, [id]);
+  const handleClose = useCallback(() => {
+    setOpen(false);
+    setId(null);
+  }, [id]);
 
-    const {ADD_PROJECT, GET_PROJECTS, GET_PROJECTID, UPDATE_PROJECT} =
-        projects();
-    const {GET_CLIENTS} = clients();
+  const { ADD_PROJECT, GET_PROJECTS, GET_PROJECTID, UPDATE_PROJECT } =
+    projects();
+  const { GET_CLIENTS } = clients();
 
-    const {data: projectList, loading} = useQuery<{ project: IProject }>(
-        GET_PROJECTID,
-        {
-            variables: {
-                id,
-            },
-            skip: id === null,
-            ssr: true,
-        }
-    );
-    const {data: clientsList} = useQuery<{ clients: IClients[] }>(GET_CLIENTS, {
-        ssr: true,
-    });
-    const {data: projectsList} = useQuery(GET_PROJECTS, {ssr: true});
+  const { data: projectList, loading } = useQuery<{ project: IProject }>(
+    GET_PROJECTID,
+    {
+      variables: {
+        id,
+      },
+      skip: id === null,
+      ssr: true,
+    }
+  );
+  const { data: clientsList } = useQuery<{ clients: IClients[] }>(GET_CLIENTS, {
+    ssr: true,
+  });
+  const { data: projectsList } = useQuery(GET_PROJECTS, { ssr: true });
 
-    const [updateProject] = useMutation(UPDATE_PROJECT, {
-        variables: {
-            id,
-            ...(clientId !== null ? {clientId} : {}),
-            ...(name !== null ? {name} : {}),
-            ...(description !== null ? {description} : {}),
-            ...(status !== null ? {status} : {}),
-        },
-    });
+  const [updateProject] = useMutation(UPDATE_PROJECT, {
+    variables: {
+      id,
+      ...(clientId !== null ? { clientId } : {}),
+      ...(name !== null ? { name } : {}),
+      ...(description !== null ? { description } : {}),
+      ...(status !== null ? { status } : {}),
+    },
+  });
 
-    const [addProject] = useMutation(ADD_PROJECT, {
-        variables: {
-            clientId,
-            name,
-            description,
-            status: status ?? "new",
-        },
-        update(cache, {data: {addProject}}) {
-            const {projects} = cache.readQuery<any>({query: GET_PROJECTS});
-            cache.writeQuery({
-                query: GET_PROJECTS,
-                data: {projects: projects?.concat([addProject]) ?? []},
-            });
-        },
-        // refetchQueries: [{query: GET_PROJECTS}]
-    });
+  const [addProject] = useMutation(ADD_PROJECT, {
+    variables: {
+      clientId,
+      name,
+      description,
+      status: status ?? "new",
+    },
+    update(cache, { data: { addProject } }) {
+      const { projects } = cache.readQuery<any>({ query: GET_PROJECTS });
+      cache.writeQuery({
+        query: GET_PROJECTS,
+        data: { projects: projects?.concat([addProject]) ?? [] },
+      });
+    },
+    // refetchQueries: [{query: GET_PROJECTS}]
+  });
 
-    const handleNameChange = (event: ChangeEvent<HTMLInputElement>) => {
-        setName(event.target.value);
-    };
+  const handleNameChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  };
 
-    const handleDescriptionChange = (event: ChangeEvent<HTMLInputElement>) => {
-        setDescription(event.target.value);
-    };
+  const handleDescriptionChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setDescription(event.target.value);
+  };
 
-    const handleSubmit = async (event: FormEvent) => {
-        event.preventDefault();
-        try {
-            if (id === "" || id === null) {
-                await addProject();
-            } else {
-                await updateProject();
-            }
-            setClientId(null);
-            setName(null);
-            setDescription(null);
-            setStatus(null);
-            handleClose();
-        } catch (err) {
-            console.log("@@err", err);
-        }
-    };
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+    try {
+      if (id === "" || id === null) {
+        await addProject();
+      } else {
+        await updateProject();
+      }
+      setClientId(null);
+      setName(null);
+      setDescription(null);
+      setStatus(null);
+      handleClose();
+    } catch (err) {
+      console.log("@@err", err);
+    }
+  };
 
-    const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
-        if (event.key === "Escape") {
-            event.stopPropagation();
-        }
-    };
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    if (event.key === "Escape") {
+      event.stopPropagation();
+    }
+  };
 
-    const handleStatusChange = (event: SelectChangeEvent) => {
-        setStatus(event.target.value as string);
-    };
+  const handleStatusChange = (event: SelectChangeEvent) => {
+    setStatus(event.target.value as string);
+  };
 
-    const handleClientChange = async (event: SelectChangeEvent) => {
-        await setClientId(event.target.value as string);
-    };
+  const handleClientChange = async (event: SelectChangeEvent) => {
+    await setClientId(event.target.value as string);
+  };
 
-    const selectedStatus = useMemo(() => {
-        return (
-            clientIdOptions?.filter((x) => {
-                if (x.label === projectList?.project.status) {
-                    return x;
-                }
-            })[0]?.value ?? null
-        );
-    }, [id, projectList?.project?.status]);
-
+  const selectedStatus = useMemo(() => {
     return (
-        <>
-            <Box sx={{my: 3}}>
-                <Typography variant={"h3"}>Projects</Typography>
-            </Box>
-            <Button
-                variant={"contained"}
-                color={"primary"}
-                onClick={() => {
-                    handleOpen();
-                }}
-                sx={{mb: 2}}
-            >
-                Add Project
-            </Button>
-            {loading ? (
-                <Box>
-                    <Typography variant={"body1"}>Loading...</Typography>
-                </Box>
-            ) : (
-                <TableContainer component={Paper}>
-                    <Table sx={{minWidth: 650}} aria-label="simple table">
-                        <TableHead>
-                            <TableRow>
-                                <TableCell>id</TableCell>
-                                <TableCell>name</TableCell>
-                                <TableCell>description</TableCell>
-                                <TableCell>status</TableCell>
-                                <TableCell></TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {projectsList?.projects?.map((project: Project) => (
-                                <TableRow
-                                    key={project.id}
-                                    sx={{"&:last-child td, &:last-child th": {border: 0}}}
-                                >
-                                    <ProjectRow handleOpen={handleOpen} project={project}/>
-                                </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-            )}
-            <ProjectModal
-                id={id}
-                handleClose={handleClose}
-                open={open}
-                setClientId={setClientId}
-                setName={setName}
-                setDescription={setDescription}
-                setStatus={setStatus}
-                clientId={clientId}
-                name={name}
-                description={description}
-                status={status}
-                addProject={addProject}
-                updateProject={updateProject}
-                projectList={projectList}
-                clientsList={clientsList}
-                clientIdOptions={clientIdOptions}
-                handleKeyDown={handleKeyDown}
-                handleStatusChange={handleStatusChange}
-                handleClientChange={handleClientChange}
-                selectedStatus={selectedStatus}
-                handleNameChange={handleNameChange}
-                handleDescriptionChange={handleDescriptionChange}
-                handleSubmit={handleSubmit}
-            />
-        </>
+      clientIdOptions?.filter((x) => {
+        if (x.label === projectList?.project.status) {
+          return x;
+        }
+      })[0]?.value ?? null
     );
+  }, [id, projectList?.project?.status]);
+
+  return (
+    <>
+      <Box sx={{ my: 3 }}>
+        <Typography variant={"h3"}>Projects</Typography>
+      </Box>
+      <Button
+        variant={"contained"}
+        color={"primary"}
+        onClick={() => {
+          handleOpen();
+        }}
+        sx={{ mb: 2 }}
+      >
+        Add Project
+      </Button>
+      {loading ? (
+        <Box>
+          <Typography variant={"body1"}>Loading...</Typography>
+        </Box>
+      ) : (
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>id</TableCell>
+                <TableCell>name</TableCell>
+                <TableCell>description</TableCell>
+                <TableCell>status</TableCell>
+                <TableCell></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {projectsList?.projects?.map((project: Project) => (
+                <TableRow
+                  key={project.id}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <ProjectRow handleOpen={handleOpen} project={project} />
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      )}
+      <ProjectModal
+        id={id}
+        handleClose={handleClose}
+        open={open}
+        setClientId={setClientId}
+        setName={setName}
+        setDescription={setDescription}
+        setStatus={setStatus}
+        clientId={clientId}
+        name={name}
+        description={description}
+        status={status}
+        addProject={addProject}
+        updateProject={updateProject}
+        projectList={projectList}
+        clientsList={clientsList}
+        clientIdOptions={clientIdOptions}
+        handleKeyDown={handleKeyDown}
+        handleStatusChange={handleStatusChange}
+        handleClientChange={handleClientChange}
+        selectedStatus={selectedStatus}
+        handleNameChange={handleNameChange}
+        handleDescriptionChange={handleDescriptionChange}
+        handleSubmit={handleSubmit}
+      />
+    </>
+  );
 };
 
 export default ProjectContainer;
